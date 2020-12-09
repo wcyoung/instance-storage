9cd21db2e000c0246d649936ef0d63142a055edd
Chiyoung Won <wcyoung98@gmail.com>
2020-12-09 21:14:48 +0900

    StoredInstanceLoader 기능 추가
    생성자 파라미터의 인스턴스를 생성해서 파라미터로 사용

--- src/main/java/wcyoung/storage/instance/loader/StoredInstanceLoader.java
+++ src/main/java/wcyoung/storage/instance/loader/StoredInstanceLoader.java
@@ -3,6 +3,10 @@ package wcyoung.storage.instance.loader;
 import wcyoung.storage.instance.Storage;
 import wcyoung.storage.instance.Stored;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.stream.Stream;
+
 public class StoredInstanceLoader implements InstanceLoader {
 
     private Storage storage;
@@ -16,12 +20,43 @@ public class StoredInstanceLoader implements InstanceLoader {
         Class<?>[] classes = ClassWithAnnotationScanner.scan(basePackage, Stored.class);
 
         for (Class<?> clazz : classes) {
-            try {
-                storage.add(clazz.newInstance());
-            } catch (InstantiationException | IllegalAccessException e) {
-                throw new InstanceLoadException(clazz + " does not have a public constructor.");
-            }
+            storage.add(generateInstance(clazz));
         }
+    }
+
+    protected Object generateInstance(Class<?> clazz) {
+        Constructor<?> constructor = findConstructor(clazz);
+
+        Object[] parameters = Stream.of(constructor.getParameterTypes())
+                .map(type -> {
+                    try {
+                        return type.newInstance();
+                    } catch (InstantiationException | IllegalAccessException e) {
+                        throw new InstanceLoadException(
+                                "Parameters of constructor must have a default public constructor.");
+                    }
+                })
+                .toArray();
+
+        try {
+            return constructor.newInstance(parameters);
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException e) {
+            throw new InstanceLoadException(e);
+        }
+    }
+
+    protected Constructor<?> findConstructor(Class<?> clazz) {
+        Constructor<?>[] constructors = clazz.getConstructors();
+        if (constructors.length == 0) {
+            throw new InstanceLoadException(clazz + " does not have a public constructor.");
+        }
+
+        if (constructors.length > 1) {
+            throw new InstanceLoadException(clazz + " has too many constructors.");
+        }
+
+        return constructors[0];
     }
 
 }
