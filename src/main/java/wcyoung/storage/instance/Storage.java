80a7f4f9499df9540dccb1d9d31174a0f033a464
Chiyoung Won <wcyoung98@gmail.com>
2020-12-09 20:51:26 +0900

    인스턴스 저장소 추가

---
+++ src/main/java/wcyoung/storage/instance/Storage.java
@@ -0,0 +1,41 @@
+package wcyoung.storage.instance;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class Storage {
+
+    private static final Storage STORAGE = new Storage();
+
+    private final ConcurrentMap<Class<?>, Object> INSTANCES;
+
+    private Storage() {
+        INSTANCES = new ConcurrentHashMap<>();
+    }
+
+    public static Storage getInstance() {
+        return STORAGE;
+    }
+
+    public boolean contains(Class<?> key) {
+        return INSTANCES.containsKey(key);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T get(Class<T> key) {
+        return (T) INSTANCES.get(key);
+    }
+
+    public void add(Object value) {
+        add(value.getClass(), value);
+    }
+
+    public void add(Class<?> key, Object value) {
+        INSTANCES.put(key, value);
+    }
+
+    public void remove(Class<?> key) {
+        INSTANCES.remove(key);
+    }
+
+}
