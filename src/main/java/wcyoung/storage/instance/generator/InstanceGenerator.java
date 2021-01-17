1d9edfd3334e8fec64859c763022bbf9add8bf3f
Chiyoung Won <wcyoung98@gmail.com>
2021-01-17 23:11:46 +0900

    refactor: Add exception throw

--- src/main/java/wcyoung/storage/instance/generator/InstanceGenerator.java
+++ src/main/java/wcyoung/storage/instance/generator/InstanceGenerator.java
@@ -1,9 +1,20 @@
 package wcyoung.storage.instance.generator;
 
+import java.lang.reflect.Modifier;
+
 public class InstanceGenerator {
 
     public static <T> T generate(Class<T> clazz) {
         try {
+            int modifiers = clazz.getModifiers();
+            if (Modifier.isInterface(modifiers)) {
+                throw new InstanceGenerateException("Unable to generate " + clazz + " instance. Because it is an interface.");
+            }
+
+            if (Modifier.isAbstract(modifiers)) {
+                throw new InstanceGenerateException("Unable to generate " + clazz + " instance. Because it is an abstract class.");
+            }
+
             return clazz.newInstance();
         } catch (InstantiationException | IllegalAccessException e) {
             throw new InstanceGenerateException(
